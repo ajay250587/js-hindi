c3eccb7e859e433404b6464ed6da7bd6d26f9ea2
ajay250587 <109685380+ajay250587@users.noreply.github.com>
2023-08-02 07:24:54 +0000

    test js updated

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
-console.log("Ajay")
+const applicantId = 12345
+let applicantName = "Ajay"
+var applicantEmail = "test@test.com"
+let applicantEmail = "test@test.com"
+//console.log(applicantId);
+console.table([applicantId, applicantName, applicantEmail]);
+
+/*
+Notes:
+Prefer not to use var beacause of issue in block scope and functional scope
+*/
