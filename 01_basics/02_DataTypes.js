b4a662a1b8dcfb9f6f445d5e97d8ebdd93a20558
ajay250587 <109685380+ajay250587@users.noreply.github.com>
2023-08-19 16:49:27 +0000

    03_dataType_summery

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,18 @@
+//Primitive Data Types
+
+//1. Number=> 2 to power 53
+//2. String => ""
+//3. Boolean => true/false
+//4. Null => standalone valle
+//5. Undefined => undefined
+//6. BigInt
+//7. Symbol = unique
+//8. NaN
+
+//Reference type
+
+//1. Object
+
+console.log(typeof null) // type of null is Object
+console.log(typeof undefined) // type of undefined is undefined
+//2. Array
